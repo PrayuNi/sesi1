dd638ee5184706b769d0674e5c5262a18e0a9837
PrayuNi <127836222+PrayuNi@users.noreply.github.com>
2023-03-14 17:01:04 +0800

    Add files via upload

---
+++ indeks.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML Prayu Ni!</title>
+</head>
+<body>
+    
+    <h3>Ayo Belajar Web Programing<?="<sup>80</sup>"?></h3>
+
+    <script>
+        document.write("<h3>Menggunakan script javascript</h3>");
+    </script>
+
+<?php
+    echo "<h3>Menggunakan script PHP</h3>";
+?>
+
+</body>
+</html> 
